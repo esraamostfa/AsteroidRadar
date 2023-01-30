bb25fda7a6f35d1bc2b713128e69b9be6b99e6ba
esraamostfa <esraamostfa13@gmail.com>
2023-01-30 23:46:48 +0200

    Add MainFragment UI

--- app/src/main/java/com/esraa/egfwd/asteroidradar/ui/main/MainFragment.kt
+++ app/src/main/java/com/esraa/egfwd/asteroidradar/ui/main/MainFragment.kt
@@ -5,18 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.esraa.egfwd.asteroidradar.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
 
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
+
+
         binding = FragmentMainBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
 
         return binding.root
 
---
+++ app/src/main/java/com/esraa/egfwd/asteroidradar/ui/main/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.esraa.egfwd.asteroidradar.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel  : ViewModel() {
+}
