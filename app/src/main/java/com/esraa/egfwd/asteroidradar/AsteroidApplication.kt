ef014adb02bba5c685a78b1fed6faa279685bdad
esraamostfa <esraamostfa13@gmail.com>
2023-02-03 16:27:19 +0200

    Add Work Manager to refresh data

--- app/src/main/java/com/esraa/egfwd/asteroidradar/AsteroidApplication.kt
+++ app/src/main/java/com/esraa/egfwd/asteroidradar/AsteroidApplication.kt
@@ -1,9 +1,15 @@
 package com.esraa.egfwd.asteroidradar
 
 import android.app.Application
+import androidx.work.*
 import com.esraa.egfwd.asteroidradar.data.local.AsteroidDataBase
 import com.esraa.egfwd.asteroidradar.data.repository.AsteroidRepository
+import com.esraa.egfwd.asteroidradar.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class AsteroidApplication : Application() {
 
@@ -14,11 +20,36 @@ class AsteroidApplication : Application() {
         AsteroidRepository(dataBase = database.dao)
     }
 
-
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        delayedInit()
 
+    }
+
+    private fun delayedInit() = applicationScope.launch {
+        setupRecurringWork()
+    }
+
+    private fun setupRecurringWork() {
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                    setRequiresDeviceIdle(true)
+            }.build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest)
     }
 }
---
+++ app/src/main/java/com/esraa/egfwd/asteroidradar/work/RefreshDataWork.kt
@@ -0,0 +1,24 @@
+package com.esraa.egfwd.asteroidradar.work
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.esraa.egfwd.asteroidradar.data.repository.AsteroidRepository
+import retrofit2.HttpException
+
+class RefreshDataWorker( private val repository: AsteroidRepository, appContext: Context, params: WorkerParameters):
+    CoroutineWorker(appContext, params) {
+
+    companion object {
+        const val WORK_NAME = "RefreshDataWorker"
+    }
+
+    override suspend fun doWork(): Result {
+        return try {
+
+            repository.refreshAsteroids()
+            Result.success()
+        } catch (e: HttpException){
+            Result.retry()
+        }
+}
+    }
